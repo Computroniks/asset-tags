d57a759bfa2f80116ff2c0446766f0ea70ea4f70
Matthew Nickson <mnickson@sidingsmedia.com>
2023-01-19 19:45:42 +0000

    Added ability to add different request methods
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- router/router.go
+++ router/router.go
@@ -5,18 +5,69 @@ package router
 
 import (
 	"log"
+	"net/http"
+	"strconv"
 
-	"github.com/Computroniks/asset-tags/store"
+	"github.com/Computroniks/asset-tags/util"
 )
 
-// Create new router and register all endpoint handlers
-func RegisterHandlers (store store.Store) {
-	log.Println("Starting new router")
-	// TODO: Add handlers
+type Router struct {
+	routes map[string]map[string]func(http.ResponseWriter, *http.Request)
+	errors map[int]func(http.ResponseWriter, *http.Request)
+	basePath string
+}
+
+// Return pointer to router instance
+func New(basePath string) *Router {
+	rtr := Router{
+		basePath: basePath,
+		routes: make(map[string]map[string]func(http.ResponseWriter, *http.Request)),
+		errors: make(map[int]func(http.ResponseWriter, *http.Request)),
+	}
+	return &rtr
+}
+
+// Add GET handler
+func (o *Router) GET(path string, handler func(http.ResponseWriter, *http.Request)) {
+	if _, exists := o.routes[path]; !exists {
+		o.routes[path] = make(map[string]func(http.ResponseWriter, *http.Request))
+	}
+	o.routes[path]["GET"] = handler
+}
+
+// Add POST handler
+func (o *Router) POST(path string, handler func(http.ResponseWriter, *http.Request)) {
+	o.routes[path]["POST"] = handler
 }
 
 // Start the server
-func Start(addr string) {
+func (o *Router) Start(addr string) {
 	log.Println("Starting server on", addr)
-	// TODO: Actually start server
+	http.HandleFunc(o.basePath, o.routeRequest)
+	http.ListenAndServe(util.BindAddr, nil)
+}
+
+func (o *Router) sendError(w http.ResponseWriter, req *http.Request, status int) {
+	handler, exists := o.errors[status]
+	log.Println(req.Method, req.URL, req.RemoteAddr, status)
+	if exists {
+		handler(w, req)
+	} else {
+		w.WriteHeader(status)
+		w.Write([]byte(strconv.Itoa(status)))
+	}
+
+}
+
+// Route the users request to the correct handler
+func (o *Router) routeRequest(w http.ResponseWriter, req *http.Request) {
+	handler, exists := o.routes[req.URL.Path][req.Method]
+	if exists {
+		handler(w, req)
+	} else {
+		if _, exists := o.routes[req.URL.Path]; exists {
+			o.sendError(w, req, http.StatusMethodNotAllowed)
+		}
+		o.sendError(w, req, http.StatusNotFound)
+	}
 }
