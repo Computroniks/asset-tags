610bc8f4c4d7af5d22bd86a14ec1890f5533eff6
Matthew Nickson <mnickson@sidingsmedia.com>
2023-01-20 18:19:05 +0000

    Fixed errors always returning 200
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- handler/errors.go
+++ handler/errors.go
@@ -10,13 +10,16 @@ import (
 )
 
 func HTTP400(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
 	templates.Templates["400"].Execute(w, nil)
 }
 
 func HTTP404(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
 	templates.Templates["404"].Execute(w, nil)
 }
 
 func HTTP500(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
 	templates.Templates["500"].Execute(w, nil)
 }
